8673e5448ca3f51011715a7b599222291ed5681d
ritesh-gitwork <riteshmca.321@gmail.com>
2025-03-02 00:57:33 +0530

    part 3 completed

---
+++ Javascript/part3/condition-challenges.js
@@ -0,0 +1,54 @@
+/*
+let num1 = 6;
+let num2 = 9;
+
+if (num1 > num2) {
+  console.log("num 1 is greater than 2");
+} else {
+  console.log("num2 is greater");
+}
+
+*/
+
+// Checking if string is equal to another string
+
+let user = "ritesh";
+let anotherUser = "ritesh";
+
+if (user == anotherUser) {
+  console.log("you can't choose this username");
+} else {
+  console.log("you can choose this username");
+}
+
+// Checking a varial is number or
+
+let score = "hvbh";
+
+if (typeof score === "number") {
+  console.log("Yes this is a number");
+} else {
+  console.log("this is not a number");
+}
+
+// Checking if a bolean value is true or false
+
+let isSignedIN = true;
+
+if (isSignedIN) {
+  console.log("User signed in");
+} else {
+  console.log("not signed in");
+}
+
+//Check if array is empty or not
+
+let product = [];
+
+console.log("Length of product :", product.length);
+
+if (product.length === 0) {
+  console.log("Product is empty");
+} else {
+  console.log("product is not empty");
+}
