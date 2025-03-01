5a8f873b1a9ee5c9e1fbac56cc03d5031c3a56fb
ritesh-gitwork <riteshmca.321@gmail.com>
2025-03-01 11:42:24 +0530

    complete part1

---
+++ Javascript/part2/primitive.js
@@ -0,0 +1,35 @@
+//Number
+
+// let balance = 120;
+// let anothebalace = new Number(128);
+
+// console.log(balance);
+let balance = 120;
+let anotherBalace = new Number();
+// console.log(anothebalace);
+
+// console.log(typeof balance);
+// console.log(typeof anothebalace);
+
+// Undefined and null
+
+let firstname = null;
+let lastname = undefined;
+// console.log(firstname);
+// console.log(lastname);
+
+// String
+
+let mystr = "healloo";
+let mystr2 = "healloo   worst";
+
+let oldGreet = mystr + "ritesh"; // old backtic method
+// console.log(oldGreet);
+
+let greetMessage = `hello ${mystr}`;
+// console.log(greetMessage);
+
+let sm1 = Symbol("titesh");
+let sm2 = Symbol("ritesh");
+
+console.log(sm1 == sm2);
