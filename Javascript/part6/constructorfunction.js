fa2ca6b29e9bb67926b5e4ac787458b481ff8fd3
ritesh-gitwork <riteshmca.321@gmail.com>
2025-03-05 01:33:20 +0530

    constructor completed

--- Javascript/part6/constructorfunction.js
+++ Javascript/part6/constructorfunction.js
@@ -1,22 +1,3 @@
-/* function Person(name, age) {
-  this.name = name;
-  this.age = age;
-}
-
-function Car(make, model) {
-  this.make = make;
-  this.model = model;
-}
-
-let myCar = new Car("toyota", "Camry"); // new keyword is used to declare or make a contructor function
-console.log(myCar);
-
-let myNewCar = new Car("tata", "safari");
-console.log(myNewCar);
- */
-
-
-
 function Person(name, age) {
   this.name = name;
   this.age = age;
@@ -27,10 +8,10 @@ function Car(make, model) {
   this.model = model;
 }
 
-let myCar = new Car("Toyota", "Camry");
+let myCar = new Car("toyota", "Camry"); // new keyword is used to declare or make a contructor function
 // console.log(myCar);
 
-let myNewCar = Car("Tata", "Safari");
+let myNewCar = new Car("tata", "safari");
 // console.log(myNewCar);
 
 function Tea(type) {
@@ -39,22 +20,27 @@ function Tea(type) {
     return `this is a cup of ${this.type}`;
   };
 }
+
 let lemonTea = new Tea("lemon tea");
 // console.log(lemonTea.describe());
 
+//Another method which is used above is also done below given method
+
 function Animal(species) {
   this.species = species;
+
+  Animal.prototype.sound = function () {
+    return `${this.species} makes a sound`;
+  };
 }
 
-Animal.prototype.sound = function () {
-  return `${this.species} makes a sound`;
-};
-
-let dog = new Animal("Dog");
+let dog = new Animal("dog");
 // console.log(dog.sound());
 
 let cat = new Animal("cat");
 // console.log(cat.sound());
+
+// a little bit advanced version which specify as we have used the keyword new or not
 
 function Drink(name) {
   if (!new.target) {
@@ -62,5 +48,6 @@ function Drink(name) {
   }
   this.name = name;
 }
+
 let tea = new Drink("tea");
-let coffee = Drink("coffee");
+let coffee = new Drink("coffee");
