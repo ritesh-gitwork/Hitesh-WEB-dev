c28a1e1fece2ac6a70046694dbace890d3ad54c6
ritesh-gitwork <riteshmca.321@gmail.com>
2025-04-06 19:17:06 +0530

    todo completed

--- JS_Projects/01_ToDo_Localstorage/script.js
+++ JS_Projects/01_ToDo_Localstorage/script.js
@@ -1,5 +1,61 @@
-const todoInput = document.getElementById("todo-input");
-const addTaskButton = document.getElementById("add-task-btn");
-const todoList = document.getElementById("todo-list");
+document.addEventListener("DOMContentLoaded", () => {
+  const todoInput = document.getElementById("todo-input");
+  const addTaskButton = document.getElementById("add-task-btn");
+  const todoList = document.getElementById("todo-list");
 
-let tasks = [];
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  tasks.forEach((task) => renderTask(task));
+
+  addTaskButton.addEventListener("click", () => {
+    const taskText = todoInput.value.trim();
+    // if (taskText === "") return;
+    if (tasks.some((task) => task.text === taskText)) return; // chatgpt same input is not accepted
+
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      completed: false,
+    };
+
+    tasks.push(newTask);
+    saveTasks();
+    renderTask(newTask);
+    todoInput.value = ""; //clear input
+    console.log(tasks);
+  });
+
+  function renderTask(task) {
+    // console.log(task);
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+    if (task.completed) li.classList.add("completed");
+    li.innerHTML = `<span> ${task.text} </span> <button>delete</button>
+     `;
+
+    li.addEventListener("click", (e) => {
+      if (e.target.tagName === "BUTTON") return;
+      task.completed = task.completed;
+      li.classList.toggle("completed");
+      saveTasks();
+    });
+
+    li.querySelector("button").addEventListener("click", (e) => {
+      e.stopPropagation(); //prevent toggle from firing
+      tasks = tasks.filter((t) => t.id !== task.id);
+      li.remove();
+      saveTasks();
+    });
+
+    todoList.appendChild(li);
+
+    // emter button to add task
+    todoInput.addEventListener("keyup", (e) => {
+      if (e.key === "Enter") addTaskButton.click();
+    });
+  }
+
+  function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+});
